a55e0c36b99ad85f4b2c9b749d8f9aa83c1a1d29
Chelsea Myers <69124666+Chelsea-Myers@users.noreply.github.com>
2024-07-25 13:46:46 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Instacart_Dash_Apps
+
+Various Plotly Dash Apps created using a subset of the Instacart Dataset found on Kaggle.
+
+Inspiration drawn from the [Plotly Dash Tutorial](https://dash.plotly.com/tutorial)
---
+++ app.py
@@ -0,0 +1,37 @@
+# Install Packages
+
+# Load the data
+
+
+# Set up day of the week as a text feature
+
+
+# Group data to find number of orders per day
+
+
+#Format data for data table
+
+
+# Group data to find number of orders per hour
+
+
+#Format data for data table
+
+
+
+# Initialize the Dash app
+app = Dash(__name__)
+
+server = app.server
+
+# App layout
+
+
+# Set up callbacks
+
+
+# Create graphs
+
+    
+#Run the app
+
